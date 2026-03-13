4be402ee5c5d63dd935d741bc8125691c75329a3
Jhon Jairo Gelvez Gomez <jhonjairogelvezgomez@gmail.com>
2026-03-13 16:47:23 -0500

    comentarios añadidos

--- 8.js
+++ 8.js
@@ -4,15 +4,20 @@ let usuario = "Jhon";
 let intentos = 3;
 
 while (intentos > 0) {
+    // pedir datos al usuario
     let usuarioIngreso = prompt("ingrese su usuario");
     let contrasenaIngreso = prompt("ingrese su contrasena");
+
+    // validar que los datos ingresados sean los mismos
     if (usuario == usuarioIngreso && contrasena == contrasenaIngreso) {
+        // mostrar bienvenido
         alert("bienvenido");
         break;
     } else {
         intentos--;
     }
     if (intentos == 0) {
+        // mostrar acceso denegado
         alert("acceso denegado");
     }
 }
